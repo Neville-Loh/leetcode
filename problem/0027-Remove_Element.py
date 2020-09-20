3191b1fffaf455e8552285e774f6f96fec945c4d
Neville Loh <48974742+Neville-Loh@users.noreply.github.com>
2020-09-20 21:17:26 +1200

    Update 0027-Remove_Element.py

--- problem/0027-Remove_Element.py
+++ problem/0027-Remove_Element.py
@@ -27,3 +27,17 @@ class Solution(object):
         for i in range(len(nums)-1,-1,-1):
             if nums[i] == val:
                 nums.pop(i)
+        return len(nums)
+    
+    def removeElement2(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != val :
+                nums[count] = nums[i]
+                count +=1
+        return count
