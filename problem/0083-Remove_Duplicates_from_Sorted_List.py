8c82b29fafd8b5c6e1cd134053c1ac5415ba3626
Neville Loh <48974742+Neville-Loh@users.noreply.github.com>
2020-09-26 19:39:51 +1200

    Update 0083-Remove_Duplicates_from_Sorted_List.py

--- problem/0083-Remove_Duplicates_from_Sorted_List.py
+++ problem/0083-Remove_Duplicates_from_Sorted_List.py
@@ -1,8 +1,19 @@
 """
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+
+Input: 1->1->2
+Output: 1->2
+Example 2:
+
+Input: 1->1->2->3->3
+Output: 1->2->3
 
 
-
-
+Difficulty: Easy
+Author: Neville
+Date: 26 Sept 2020
 """
 # Definition for singly-linked list.
 # class ListNode(object):
